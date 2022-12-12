e31a054be3dfc8dbead7dd9f73019e5b7d1112d4
NBK <1justforstuding1@gmail.com>
2022-12-12 14:21:19 +0300

    add user model

---
+++ ChaTik/Startup.cs
@@ -0,0 +1,46 @@
+﻿using ChaTik.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChaTik;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+ 
+    public IConfiguration Configuration { get; }
+ 
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationContext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+ 
+        services.AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationContext>();
+                 
+        services.AddControllersWithViews();
+    }
+ 
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseDeveloperExceptionPage();
+ 
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+ 
+        app.UseRouting();
+ 
+        app.UseAuthentication();    // подключение аутентификации
+        app.UseAuthorization();
+ 
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
