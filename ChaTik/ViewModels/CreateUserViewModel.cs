8805d7b8d083b48738e13f1ca4dde86054782e4d
NBK <1justforstuding1@gmail.com>
2022-12-28 20:13:18 +0300

    add create, edit and delete users

---
+++ ChaTik/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ChaTik.ViewModels;
+
+public class CreateUserViewModel
+{
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+}
